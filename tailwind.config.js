b86e73085064c9d3d0072c5d7dfc53d1e4268b50
lb <lbdeyo@gmail.com>
2023-03-13 13:23:54 -0500

    same

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./index.html"],
+  content: ["./src/**/*.{html,js}", "./public/index.html"],
   darkMode: "class",
   theme: {
     extend: {
